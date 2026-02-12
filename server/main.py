fa98fca3ebedbf470b5f87bbe7d9e17ab7f568e4
riyakasaudhan20 <riyakasaudhan3467@gmail.com>
2026-02-13 01:15:08 +0530

    first commit

--- server/main.py
+++ server/main.py
@@ -18,4 +18,4 @@ async def startup_event():
 
 if __name__ == "__main__":
   logger.info("Running app...")
-  uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
